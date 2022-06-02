004ee2529fb5fbce12e926bdda4cb95625c3a989
Bryan Stidh Calero Giraldo <bryan.calero@pragma.com.co>
2022-06-02 14:37:57 -0500

    primer commit

---
+++ Shared/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  Shared
+//
+//  Created by Bryan Calero on 1/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
